ffda7f5bcbbdf0619fe07f3d7baa56eec0d82116
Dmitry <d1mon227@yandex.ru>
2023-04-16 21:03:13 +0300

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Dmitry Medvedev on 16.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
